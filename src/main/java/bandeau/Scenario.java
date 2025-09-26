939dd8a374f0b0c458d2c5dc28551052f5b913d7
elsamlt <elsa.milhet@etud.univ-jfc.fr>
2025-09-26 12:25:47 +0200

    Update Scenario.java

--- src/main/java/bandeau/Scenario.java
+++ src/main/java/bandeau/Scenario.java
@@ -1,6 +1,6 @@
 package bandeau;
-import java.util.List;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Classe utilitaire pour représenter la classe-association UML
@@ -33,16 +33,101 @@ public class Scenario {
         myElements.add(new ScenarioElement(e, repeats));
     }
 
+    // Vérifie si la liste des éléments du scénario est vide
+    public boolean isEmpty() {
+        return myElements.isEmpty();
+    }
+
     /**
      * Jouer ce scenario sur un bandeau
      *
      * @param b le bandeau ou s'afficher.
      */
     public void playOn(Bandeau b) {
-        for (ScenarioElement element : myElements) {
-            for (int repeats = 0; repeats < element.repeats; repeats++) {
-                element.effect.playOn(b);
+        // for (ScenarioElement element : myElements) {
+        //     for (int repeats = 0; repeats < element.repeats; repeats++) {
+        //         element.effect.playOn(b);
+        //     }
+        // }
+
+        // V1 - On doit jouer le scénario en même temps sur plusieurs bandeaux (intégrer dans le playOn):
+        // Thread t = new Thread() {
+        //     @Override
+        //     public void run() {
+        //         for (ScenarioElement element : myElements) {
+        //             for (int repeats = 0; repeats < element.repeats; repeats++) {
+        //                 element.effect.playOn(b); 
+        //             }
+        //         }
+        //     }
+        // };
+
+        // V2 - On ne doit pas jouer plusieurs scénarios en même temps sur le même bandeau (synchronisation)
+        // Thread t2 = new Thread() {
+        //     @Override
+        //     public void run() {
+        //         synchronized (b) { 
+        //             for (ScenarioElement element : myElements) {
+        //                 for (int repeats = 0; repeats < element.repeats; repeats++) {
+        //                     element.effect.playOn(b);  
+        //                 }
+        //             }
+        //         }
+        //     }
+        // };
+
+        // V3 - On ne doit pas modifier un scénario s'il est en train d'être joué dans un bandeau        
+        // Thread t3 = new Thread() {
+        //     @Override
+        //     public void run() {
+        //         synchronized (b) {
+        //         // Attendre que la pile ne soit pas vide
+        //         while(isEmpty()) {
+        //             b.wait();
+        //         }
+
+        //         for (ScenarioElement element : myElements) {
+        //             for (int repeats = 0; repeats < element.repeats; repeats++) {
+        //                 element.effect.playOn(b);  
+        //             }
+        //         }
+                    
+        //         Thread.sleep(10000);
+        //         }
+        //         synchronized (b) {
+        //             b.notifyAll();
+        //         }
+        //     }
+        // };
+
+        Thread t3 = new Thread() {
+            @Override
+            public void run() {
+                synchronized (b) { 
+                    try {
+                        while (isEmpty()) {
+                            b.wait();  
+                        }
+
+                        for (ScenarioElement element : myElements) {
+                            for (int repeats = 0; repeats < element.repeats; repeats++) {
+                                element.effect.playOn(b);
+                            }
+                        }
+
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    } finally {
+                        b.notifyAll();
+                    }
+                }
             }
-        }
+        };
+
+        // Démarrer les deux threads
+        // t.start();
+        // t2.start();
+        t3.start();
     }
 }
